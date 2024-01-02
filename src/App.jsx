963006772429adf9f5687fbe80ae9e99f71e9f9d
Sachin K Thakur <60362013+sachinkthakur60@users.noreply.github.com>
2024-01-02 10:13:46 +0000

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-full bg-zinc-800'>
+      <h1 className='text-xl'>Docs</h1>
+    </div>
+  )
+}
+
+export default App
